8493eda7b5364f7b68f03b35df0769dd44882c95
Victor Rosa <vicrosa25@gmail.com>
2025-05-16 20:11:01 +0200

    Connection to db done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/vicrosa25/comment-api/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // application
 func Run() error {
 	fmt.Println("Starting application...")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged to database")
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vicrosa25/comment-api
 
 go 1.24
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
