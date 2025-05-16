e37d50fed9e7c3c4833874401f51e8c8844d73f0
Victor Rosa <vicrosa25@gmail.com>
2025-05-16 18:55:21 +0200

    Added Dockerfile

---
+++ internal/comment/comment.go
@@ -0,0 +1,62 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
+)
+
+// Comment - a representation of the comment
+// structure for our service
+type Comment struct {
+	ID     int
+	Slug   string
+	Body   string
+	Author string
+}
+
+// Store - defines all the methods that our
+// service will need to operate.
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - is the struct on which all our logic
+// will be built on top of
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("Retrieving comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, ErrFetchingComment
+	}
+	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
